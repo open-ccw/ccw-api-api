641f955e53ccc7c4accdf5000a9428a56ee6852f
BenPaoDeXiaoZhi <mengfanzhi1232022@163.com>
2026-08-09 08:51:00 +0800

    2.0.0 breaking change: student profile支持传入studentNumber

--- src/community-web/students/profile.test.ts
+++ src/community-web/students/profile.test.ts
@@ -5,6 +5,18 @@ import assert from "node:assert/strict";
 const STUDENT_OID = "63c2807d669fa967f17f5559";
 
 test("get student profile", async () => {
-  const profile = await getStudentProfile(STUDENT_OID);
-  assert.strictEqual(profile.studentOid, (STUDENT_OID), "expected values to be strictly equal");
+  const profile = await getStudentProfile({ studentOid: STUDENT_OID });
+  assert.strictEqual(
+    profile.studentOid,
+    STUDENT_OID,
+    "expected values to be strictly equal",
+  );
+  const profile2 = await getStudentProfile({
+    studentNumber: profile.studentNumber,
+  });
+  assert.strictEqual(
+    profile2.studentOid,
+    STUDENT_OID,
+    "expected values to be strictly equal",
+  );
 });
--- src/community-web/students/profile.ts
+++ src/community-web/students/profile.ts
@@ -5,7 +5,8 @@ import { StudentProfile } from "src/types/userData";
 export const url = "https://community-web.ccw.site/students/profile";
 
 export type Req = {
-  studentOid: MongoDBId;
+  studentNumber?: string;
+  studentOid?: MongoDBId;
 };
 
 export type Res = StudentProfile;
@@ -15,8 +16,14 @@ export type Res = StudentProfile;
  * @param {MongoDBId} studentOid 学生id
  * @returns {Promise<Res>} 学生资料
  */
-export async function getStudentProfile(studentOid: MongoDBId): Promise<Res> {
-  const req: Req = { studentOid };
+export async function getStudentProfile({
+  studentOid,
+  studentNumber,
+}: {
+  studentNumber?: string;
+  studentOid?: MongoDBId;
+}): Promise<Res> {
+  const req: Req = { studentOid, studentNumber };
   return await request
     .post<ApiResponse<Res>>(url, req)
     .then((res) => res.data.body);
