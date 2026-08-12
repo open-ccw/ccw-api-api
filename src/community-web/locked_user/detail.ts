45c66df38cce76fa8255d1b5d771d4562b8efa44
MengFuziComing~ <121215648+BenPaoDeXiaoZhi@users.noreply.github.com>
2026-08-12 14:54:30 +0800

    修改被封禁用户类型，便于类型推断

--- src/community-web/locked_user/detail.ts
+++ src/community-web/locked_user/detail.ts
@@ -8,30 +8,44 @@ export type Req = {
   accountOid: MongoDBId;
 };
 
-export interface LockedUserDetail<IsLocked extends boolean> {
-  accountOid: IsLocked extends true ? MongoDBId : null;
-  accountType: IsLocked extends true ? AccountTypes : null;
-  createdAt: IsLocked extends true ? number : null;
-  description: IsLocked extends true ? string : null;
-  id: IsLocked extends true ? number : null;
-  locked: IsLocked;
-  operatorOid: IsLocked extends true ? MongoDBId : null;
-  unlocksAt: IsLocked extends true ? number : null;
-  updatedAt: IsLocked extends true ? number : null;
+interface UserNotLocked {
+  accountOid: null;
+  accountType: null;
+  createdAt: null;
+  description: null;
+  id: null;
+  locked: false;
+  operatorOid: null;
+  unlocksAt: null;
+  updatedAt: null;
 }
 
-export type Res<IsLocked extends boolean> = LockedUserDetail<IsLocked>;
+interface UserLocked {
+  accountOid: MongoDBId;
+  accountType: AccountTypes;
+  createdAt: number;
+  description: string|null;
+  id: number;
+  locked: true;
+  operatorOid: MongoDBId;
+  unlocksAt: number;
+  updatedAt: number;
+}
+
+export type LockedUserDetail = UserLocked | UserNotLocked;
+
+export type Res = LockedUserDetail;
 
 /**
  * 获取被封禁用户详情
  * @param {MongoDBId} accountOid 用户账户ID
  * @returns {Promise<Res>} 被封禁用户详情
  */
-export async function getLockedUserDetail<IsLocked extends boolean = false>(
+export async function getLockedUserDetail(
   accountOid: MongoDBId,
-): Promise<Res<IsLocked>> {
+): Promise<Res> {
   const req: Req = { accountOid };
   return await request
-    .post<ApiResponse<Res<IsLocked>>>(url, req)
+    .post<ApiResponse<Res>>(url, req)
     .then((res) => res.data.body);
 }
